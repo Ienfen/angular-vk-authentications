fe3643a6a30d333619b1449ed2f41750a292d5d1
Ihor <vreysu@mail.ru>
2016-08-18 17:31:05 +0300

    init

--- app/components/list/list.controller.js
+++ app/components/list/list.controller.js
@@ -2,7 +2,15 @@
  * Created by ihor on 8/17/16.
  */
 angular.module('vk.list')
-    .controller('list.ctrl', [function() {
+    .controller('list.ctrl', ['$q',function($q) {
 
-        console.log('sad');
+        function vkApiWrapper (method, params) {
+            var d = $q.defer();
+
+            VK.api.call(VK, method, params, d.resolve.bind(d));
+
+            return d.promise;
+        }
+
+        console.log($q);
     }]);
